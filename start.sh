d165092a4c34ae17fb28c17c0ebe278788d73692
Alan Boris <39385685+alanb128@users.noreply.github.com>
2025-11-06 22:13:03 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run a script to set up and start UDEV
+# The script will then start the Hailo setup script
+./entry.sh
+
+sleep 5
+
+udevadm control --reload
+
+# If you want to start your own app, you can start it here!
+
+sleep infinity
